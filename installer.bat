99ab7115447d340a0fa0529cbe43f045f467cad2
Mantiee <143137429+Mantiee@users.noreply.github.com>
2025-06-13 23:18:33 +0200

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -2,7 +2,7 @@
 python -m pip install --upgrade pip
 
 :: Instalacja bibliotek
-pip install opencv-python numpy insightface onnxruntime-gpu keyboard
+pip install opencv-python numpy insightface onnxruntime-gpu keyboard colorama
 
 :: Dodatkowe biblioteki dla działania insightface
 pip install scipy tqdm psutil requests
